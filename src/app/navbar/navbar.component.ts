2cd41d82abcc3abbde5742f5d67dc8a00bdd41c1
Pavle Dimitrijevic <dimitrijevicc.p@gmail.com>
2025-08-30 15:58:26 +0200

    Navbar, home, images update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  isSticky: boolean = false;
 
+  headerHeight: number = 1;
+
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    const header = document.querySelector('.home__header') as HTMLElement;
+    if (header) {
+      this.headerHeight = header.offsetHeight;
+    }
+  }
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isSticky = window.scrollY >= this.headerHeight;
+  }
 }
